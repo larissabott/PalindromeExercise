92c011822473e304b7ffd2f97a57d008b80cfa50
[larissa bottefield] <[larissabott3@gmail.com]>
2020-11-23 18:45:41 -0600

    racecar

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
+using ExampleForPalindrome;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace Palindrome.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void MyTest(string word, bool expected)
         {
-
+            //arrange
+            var tester = new WordSmith();
+            //act
+            var actual = tester.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ExampleForPalindrome
+{ 
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //reverse the word //racecar Length--> 7 --> 6
+            var reversed = "";
+            for(int i = word.Length -1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            //compare the original with the reversed
+            if(reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
